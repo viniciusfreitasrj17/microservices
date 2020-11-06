fcf2bdc784a59eec2f34fad336941a771ceb1a8e
Marcos Vinicius <viniciusfreitasrj17@gmail.com>
2020-11-05 23:25:54 -0300

    🎉 Finish Basic NicroService with Golang

---
+++ b/b.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+type Result struct {
+	Status string
+}
+
+func main() {
+	http.HandleFunc("/", home)
+	http.ListenAndServe(":9091", nil)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	coupon := r.PostFormValue("coupon")
+	ccNumber := r.PostFormValue("ccNumber")
+
+	resultCoupon := makeHTTPCall("http://localhost:9092", coupon)
+
+	result := Result{Status: "declined"}
+
+	if ccNumber == "1" {
+		result.Status = "approved"
+	}
+
+	if resultCoupon.Status == "invalid" {
+		result.Status = "Invalid Coupon"
+	}
+
+	jsonData, err := json.Marshal(result)
+	if err != nil {
+		log.Fatal("Error processing json")
+	}
+
+	fmt.Fprint(w, string(jsonData))
+}
+
+func makeHTTPCall(urlMicroservice string, coupon string) Result {
+	values := url.Values{}
+	values.Add("coupon", coupon)
+
+	res, err := http.PostForm(urlMicroservice, values)
+	if err != nil {
+		result := Result{Status: "Servidor fora do ar!"}
+		return result
+	}
+
+	defer res.Body.Close()
+
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal("Error processing result")
+	}
+
+	result := Result{}
+
+	json.Unmarshal(data, &result)
+
+	return result
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/viniciusfreitasrj17/microservices
+
+go 1.15
+
+require github.com/hashicorp/go-retryablehttp v0.6.8
